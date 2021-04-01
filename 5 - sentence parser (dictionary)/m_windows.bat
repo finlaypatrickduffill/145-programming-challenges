44991030ef0171544b8d752d48d217378470d320
finlaypatrickduffill <81073306+finlaypatrickduffill@users.noreply.github.com>
2021-04-01 11:31:29 +0100

    Delete m_windows.bat

--- 5 - sentence parser (dictionary)/m_windows.bat
+++
@@ -1,7 +0,0 @@
-chdir C:\mingw\bin
-
-gcc "C:\Documents and Settings\User\Desktop\Proj\programming challenges\dictionary\mwindowed.c" -o"C:\Documents and Settings\User\Desktop\Proj\programming challenges\dictionary\mwindowed.exe" -mwindows
-
-chdir C:\Documents and Settings\User\Desktop\Proj\programming challenges\dictionary
-
-mwindowed.exe
