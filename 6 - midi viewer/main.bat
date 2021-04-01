87429da4e48cfea18fd122bf5a902a4cbae39e75
finlaypatrickduffill <81073306+finlaypatrickduffill@users.noreply.github.com>
2021-04-01 11:29:43 +0100

    Delete main.bat

--- 6 - midi viewer/main.bat
+++
@@ -1,4 +0,0 @@
-set PDCURSES_SRCDIR=C:\mingw\lib
-path=c:\mingw\bin
-gcc -O2 -Wall -L"C:\pdcurs33" "C:\Documents and Settings\User\Desktop\Proj\programming challenges\midi player\curseui.c" -o"C:\Documents and Settings\User\Desktop\Proj\programming challenges\midi player\main.exe" -lpdcurses -w
-chdir "C:\Documents and Settings\User\Desktop\Proj\programming challenges\midi player"
