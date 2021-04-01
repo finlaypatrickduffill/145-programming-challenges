638ceda4dbcdcbda0974dd6bebca6ac7efde693d
finlaypatrickduffill <81073306+finlaypatrickduffill@users.noreply.github.com>
2021-04-01 11:30:24 +0100

    Delete markov.bat

--- 4 - markov chain/markov.bat
+++
@@ -1,4 +0,0 @@
-chdir C:\mingw\bin
-gcc "C:\Documents and Settings\User\Desktop\Proj\Programming Challenges\4\markov.c" -o"C:\Documents and Settings\User\Desktop\Proj\Programming Challenges\4\markov.exe"
-chdir C:\Documents and Settings\User\Desktop\Proj\Programming Challenges\4\
-markov.exe
